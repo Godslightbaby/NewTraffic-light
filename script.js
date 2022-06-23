95ad91ad39f5f52327683a9587e5633504e5199b
Godslightbaby <103425563+Godslightbaby@users.noreply.github.com>
2022-06-23 10:14:53 -0700

    added script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let traffic = prompt("traffic Light")
+let color1 = "red"
+let color2 = "yellow"
+let color3 = "green"
+
+if (color1==traffic){
+    alert("stop")
+}
+else if(color2==traffic){
+    alert("get ready")
+}
+ else if (color3==traffic){
+    alert("GO")
+}
+ else{
+    alert("think again")
+}
